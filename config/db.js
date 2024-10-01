09c055eedebfd9b4b69123821ebe0ee7f5c85d02
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2024-10-01 10:32:17 +0200

    init : init sequelize

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+// Initialize Sequelize
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+module.exports = sequelize;
