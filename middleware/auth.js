9042268b057099688b9acd30278cec72792ffdd7
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2024-10-01 10:36:48 +0200

    feat : added middleware for auth

---
+++ middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const verifyToken = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+const checkRole = (role) => {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res
+        .status(403)
+        .json({ message: "Access denied: insufficient permissions" });
+    }
+    next();
+  };
+};
+
+module.exports = { verifyToken, checkRole };
