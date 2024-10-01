689c553e5473815ac542988250341049bea3d327
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2024-10-01 10:50:16 +0200

    init : server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const sequelize = require("./config/db");
+const authRoutes = require("./routes/auth");
+
+// Load environment variables
+dotenv.config();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Sync database and start server
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synced successfully");
+    app.listen(3000, () => {
+      console.log("Server is running on http://localhost:3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to sync database:", err);
+  });
